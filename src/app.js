aa5201cbd9ac1d8c8384693f77f27233c9ba5cfe
Jose Luis <jluisito302@gmail.com>
2023-04-12 20:12:07 -0600

    Large limit express json

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ app.set('port',config.apiPort);
 const corsOptions={};
 app.use(cors());
 //app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb',extended: false}));
 
 //ROUTE GET DEFAULT
 app.get('/', (req,res)=>{
